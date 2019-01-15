e04daaa44aaa095673f9d9126ff02d56732435bc
Tom McClintock <tmcclintock89@gmail.com>
2019-01-15 11:55:52 -0500

    Implemented the part that builds and trains the emulator.

--- boltzmulator/boltzmulator.py
+++ boltzmulator/boltzmulator.py
@@ -1,6 +1,17 @@
+import copy
 import numpy as np
 import george
-import george.kernels as ker
+from george.kernels import ExpSquaredKernel, Matern52Kernel, \
+    ExpKernel, RationalQuadraticKernel, Matern32Kernel
+import scipy.optimize as op
+
+#Assert statements to guarantee the linter doesn't complain
+assert ExpSquaredKernel
+assert Matern52Kernel
+assert ExpKernel
+assert Matern32Kernel
+assert RationalQuadraticKernel
+
 
 class boltzmulator(object):
     """
@@ -19,7 +30,8 @@ class boltzmulator(object):
             evaluated at each location in parameter space.
     """
 
-    def __init__(self, parameters, redshifts, k, power_spectra):
+    def __init__(self, parameters, redshifts, k, power_spectra,
+                 number_of_principle_components=1, kernel=None):
         parameters = np.array(parameters)
         redshifts = np.array(redshifts)
         k = np.array(k)
@@ -41,10 +53,91 @@ class boltzmulator(object):
         self.redshifts = redshifts
         self.k = k
         self.power_spectra = power_spectra
+        self.Npars = len(self.parameters[0])
 
+        self.NPC = number_of_principle_components
+        metric_guess = np.std(self.parameters, 0)
+        if kernel is None:
+            kernel = 1.*ExpSquaredKernel(metric=metric_guess, ndim=self.Npars)
+        self.kernel = kernel
+        
+    def train(self):
+        """
+        Train the emulator.
+        """
+        zs = self.redshifts
+        k = self.k
+        p = self.power_spectra
+        k2p = copy.deepcopy(p)
+        Nk = len(k)
+        Nz = len(zs)
+        #Multiply each P(k) by k^2, but note the shapes
+        #of the power spectra array we have to deal with
+        for i in range(Nz):
+            lo = i*Nk
+            hi = (i+1)*Nk
+            k2p[:, lo:hi] *= k**2
+        #Take the log
+        lnk2p = np.log(k2p)
+        #Remove the mean
+        lnk2p_mean = np.mean(lnk2p)
+        lnk2p_std = np.std(lnk2p, 0)
+        lnk2p = (lnk2p - lnk2p_mean)/lnk2p_std
+        #Save what we have noe
+        self.lnk2p = lnk2p
+        self.lnk2p_mean = lnk2p_mean
+        self.lnk2p_std = lnk2p_std
+
+        #Do SVD to pull out principle components
+        u,s,v = np.linalg.svd(lnk2p, 0) #Do the PCA
+        s = np.diag(s)
+        N = len(s)
+        P = np.dot(v.T, s)/np.sqrt(N)
+        Npc = self.NPC #number of principle components
+        phis = P.T[:Npc]
+        ws = np.sqrt(N) * u.T[:Npc]
+        #Save the weights and PCs
+        self.ws = ws
+        self.phis = phis
+
+        #Create the GPs and save them
+        gplist = []
+        for i in range(Npc):
+            ws = self.ws[i, :]
+            kern = copy.deepcopy(self.kernel)
+            gp = george.GP(kernel=kern, fit_kernel=True, mean=np.mean(ws))
+            gp.compute(self.parameters)
+            gplist.append(gp)
+            continue
+        self.gplist = gplist
+
+        #Train the GPs
+        for i, gp in enumerate(self.gplist):
+            ws = self.ws[i, :]
+            def nll(p):
+                gp.set_parameter_vector(p)
+                ll = gp.log_likelihood(ws, quiet=True)
+                return -ll if np.isfinite(ll) else 1e25
+            def grad_nll(p):
+                gp.set_parameter_vector(p)
+                return -gp.grad_log_likelihood(ws, quiet=True)
+            p0 = gp.get_parameter_vector()
+            result = op.minimize(nll, p0, jac=grad_nll)
+            gp.set_parameter_vector(result.x)
+            continue
+        
+        self.trained=True
+        return
+
+    def predict(self, parameters):
+        """
+        Predict the power spectrum at a set of cosmological parameters.
+        """
+        
 if __name__ == "__main__":
     params = [[0], [1]]
     k = [2, 3]
     z = [4, 5]
     P = [[6, 7, 8, 9], [10, 11, 12, 13]]
     b = boltzmulator(params, k, z, P)
+    b.train()
